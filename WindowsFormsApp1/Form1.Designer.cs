a756924e59109cdf465f398f1da758c17921d831
FRANCO <riccardo.arrigoni19@gmail.com>
2023-03-10 13:21:06 +0100

    Aggiungere i file di progetto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Elimina = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Visualizza = new System.Windows.Forms.Button();
+            this.Scrittaprodotto = new System.Windows.Forms.TextBox();
+            this.Modificare = new System.Windows.Forms.TextBox();
+            this.Modificato = new System.Windows.Forms.TextBox();
+            this.Prodotto = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.MAX = new System.Windows.Forms.Button();
+            this.MIN = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Aggiungi";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Elimina
+            // 
+            this.Elimina.Location = new System.Drawing.Point(12, 84);
+            this.Elimina.Name = "Elimina";
+            this.Elimina.Size = new System.Drawing.Size(75, 23);
+            this.Elimina.TabIndex = 1;
+            this.Elimina.Text = "Elimina";
+            this.Elimina.UseVisualStyleBackColor = true;
+            this.Elimina.Click += new System.EventHandler(this.Elimina_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(15, 121);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Modifica";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Visualizza
+            // 
+            this.Visualizza.Location = new System.Drawing.Point(15, 265);
+            this.Visualizza.Name = "Visualizza";
+            this.Visualizza.Size = new System.Drawing.Size(75, 23);
+            this.Visualizza.TabIndex = 3;
+            this.Visualizza.Text = "Visualizza";
+            this.Visualizza.UseVisualStyleBackColor = true;
+            this.Visualizza.Click += new System.EventHandler(this.Visualizza_Click);
+            // 
+            // Scrittaprodotto
+            // 
+            this.Scrittaprodotto.Location = new System.Drawing.Point(12, 25);
+            this.Scrittaprodotto.Name = "Scrittaprodotto";
+            this.Scrittaprodotto.Size = new System.Drawing.Size(100, 20);
+            this.Scrittaprodotto.TabIndex = 4;
+            this.Scrittaprodotto.TextChanged += new System.EventHandler(this.Scrittaprodotto_TextChanged);
+            // 
+            // Modificare
+            // 
+            this.Modificare.Location = new System.Drawing.Point(119, 124);
+            this.Modificare.Name = "Modificare";
+            this.Modificare.Size = new System.Drawing.Size(100, 20);
+            this.Modificare.TabIndex = 8;
+            // 
+            // Modificato
+            // 
+            this.Modificato.Location = new System.Drawing.Point(252, 124);
+            this.Modificato.Name = "Modificato";
+            this.Modificato.Size = new System.Drawing.Size(100, 20);
+            this.Modificato.TabIndex = 11;
+            this.Modificato.TextChanged += new System.EventHandler(this.Modificato_TextChanged);
+            // 
+            // Prodotto
+            // 
+            this.Prodotto.AutoSize = true;
+            this.Prodotto.Location = new System.Drawing.Point(12, 9);
+            this.Prodotto.Name = "Prodotto";
+            this.Prodotto.Size = new System.Drawing.Size(47, 13);
+            this.Prodotto.TabIndex = 12;
+            this.Prodotto.Text = "Prodotto";
+            this.Prodotto.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(127, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Prezzo";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(116, 108);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "A prodotto";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(249, 108);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 13);
+            this.label5.TabIndex = 17;
+            this.label5.Text = "A prezzo";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(130, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 20;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(418, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(340, 409);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 165);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 23);
+            this.button2.TabIndex = 24;
+            this.button2.Text = "Totale prezzo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // MAX
+            // 
+            this.MAX.Location = new System.Drawing.Point(15, 194);
+            this.MAX.Name = "MAX";
+            this.MAX.Size = new System.Drawing.Size(106, 23);
+            this.MAX.TabIndex = 27;
+            this.MAX.Text = "Prezzo massimo";
+            this.MAX.UseVisualStyleBackColor = true;
+            this.MAX.Click += new System.EventHandler(this.MAX_Click);
+            // 
+            // MIN
+            // 
+            this.MIN.Location = new System.Drawing.Point(15, 223);
+            this.MIN.Name = "MIN";
+            this.MIN.Size = new System.Drawing.Size(106, 23);
+            this.MIN.TabIndex = 28;
+            this.MIN.Text = "Prezzo minimo";
+            this.MIN.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MIN);
+            this.Controls.Add(this.MAX);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Prodotto);
+            this.Controls.Add(this.Modificato);
+            this.Controls.Add(this.Modificare);
+            this.Controls.Add(this.Scrittaprodotto);
+            this.Controls.Add(this.Visualizza);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Elimina);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button Elimina;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button Visualizza;
+        private System.Windows.Forms.TextBox Scrittaprodotto;
+        private System.Windows.Forms.TextBox Modificare;
+        private System.Windows.Forms.TextBox Modificato;
+        private System.Windows.Forms.Label Prodotto;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button MAX;
+        private System.Windows.Forms.Button MIN;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+        public partial class Form1 : Form
+        {   //variabili
+            public int dim;
+            public string[] arr;
+        public string[] arr2;
+        public Form1()
+            {
+                InitializeComponent();
+                arr = new string[100];
+                arr2 = new string[100];
+                dim = 0;
+
+                 for(int i = 0; i < arr.Length; i++)
+            {
+                arr2[i] = "";
+            }
+            }
+
+          
+
+        private void listView2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Scrittaprodotto_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            aggiunta(arr,arr2, ref dim, Scrittaprodotto.Text,textBox1.Text);
+            Scrittaprodotto.Text = "";
+            Scrittaprodotto.Focus();
+            textBox1.Text = "";
+            textBox1.Focus();
+            MessageBox.Show("aggiunto");
+
+        }
+
+        private void Elimina_Click(object sender, EventArgs e)
+        {
+            Rimosso(arr,arr2, ref dim);
+            Scrittaprodotto.Text = "";
+            Scrittaprodotto.Focus();
+            textBox1.Text = "";
+            textBox1.Focus();
+            MessageBox.Show("Rimosso.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Modifica(arr,arr2, ref dim, Scrittaprodotto.Text, textBox1.Text,Modificare.Text, Modificato.Text);
+            Scrittaprodotto.Text = "";
+            Scrittaprodotto.Focus();
+            textBox1.Text = "";
+            textBox1.Focus();
+            Modificare.Text = "";
+            Modificare.Focus();
+            Modificato.Text = "";
+            Modificato.Focus(); 
+            MessageBox.Show("Prodotto modificato.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+                Prezztot(arr2); 
+                MessageBox.Show("Sconto applicato");
+        }
+
+        private void MAX_Click(object sender, EventArgs e)
+        {
+            Prezzmax(arr1,arr2);
+        }
+
+        private void MIN_Click(object sender, EventArgs e)
+        {
+            Prezzmin(arr2);
+        }
+
+        //funzioni di servizio
+        public void aggiunta(string[] arr, string[] arr2, ref int dim, string nome,string r)
+        {
+            arr[dim] = nome ;
+            arr2[dim] = r;
+            dim++;
+        }
+
+        public void Rimosso(string[] arr, string[] arr2, ref int dim)
+        {
+            for (int i = 0; i < dim; i++)
+            {
+
+                if (arr[i] == Scrittaprodotto.Text)
+                {
+                    for (int j = i; j < dim; j++)
+                    {
+                        arr[j] = arr[j + 1];
+                    }
+                }
+
+                if (arr2[i] == textBox1.Text)
+                {
+                    for (int j = i; j < dim; j++)
+                    {
+                        arr2[j] = arr2[j + 1];
+                    }
+                }
+            }
+
+        }
+
+        public void Modifica(string[] arr,string [] arr2, ref int dim,string a,string b,string r,string g)
+        {
+            for (int i = 0; i < dim; i++)
+            {
+
+                if (arr[i] == a)
+                {
+                    arr[i] = r;
+                }
+
+                if (arr2[i] == b)
+                {
+                    arr2[i] = g;
+                } 
+            }
+        }
+
+        public void aggiornaVista(int dim)
+        {
+            listView1.Items.Clear();
+
+            for (int i = 0; i < dim; i++)
+            {
+                if (string.IsNullOrEmpty(arr[i]) == true)
+                {
+                    listView1.Items.Add(" ");
+                }
+                else
+                {
+                    listView1.Items.Add(prodString(arr[i],arr2[i]));
+                }
+            }
+        }
+
+        public void Prezztot(string [] arr2)
+        {
+            listView1.Items.Clear();
+
+            int a = 0;
+            int x = 0;
+
+            int dim=100;
+
+
+            for(int i=0;i<dim;i++)
+            {
+                if (arr2[i] != "")
+                {
+
+                    x = Int32.Parse(arr2[i]);
+
+                    a = a + x;
+                }
+                else
+                {
+                    dim = 0;
+                }
+
+            }
+
+            listView1.Items.Add("Prezzo totale = " + a);
+
+        }
+
+        public void Prezzmax(string[] arr1,string[] arr2)
+        {
+            float max = arr2[0].arr1;
+
+            for (int i = 0; i < max; i++)
+            {
+                if (arr2[i] > max)
+                {
+                    max = arr2[i].Length;
+                }
+            }
+
+            listView1.Items.Clear();
+            listView1.Items.Add("Prezzo minimo = " + a);
+
+        }
+
+        public void Prezzmin(string[] arr2)
+        {
+            float min = arr2[0].Length;
+
+            for (int i = 0; i < min; i++)
+            {
+                min = arr2[i].Length;
+            }
+
+                    listView1.Items.Clear();
+                    listView1.Items.Add("Prezzo minimo = " + a);
+
+
+
+
+        }
+
+
+
+        public string prodString(string p,string c)
+        {
+            return "Nome: " + p + " prezzo: " + c;
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Visualizza_Click(object sender, EventArgs e)
+        {
+            aggiornaVista(dim);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Modificato_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
